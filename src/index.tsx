bd10818f6ec48cfe04f391596021de632a3d1f1a
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-11-07 23:35:52 +0300

    123

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./s1-main/App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./s2-homeworks/hw10/bll/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/*для дз 10*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
